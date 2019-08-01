7a77c038fef582d61327738d7a56ddaeb3ed7a2b
Ben Falkenburg <46232936+Peter-Pseudonym@users.noreply.github.com>
2019-08-01 11:10:08 -0700

    Update tumorAnalysis.m

--- tumorAnalysis.m
+++ tumorAnalysis.m
@@ -1,3 +1,5 @@
+clear all
+close all
 
 %% Get User Files
 filePath = [cd, '/Tumor Results/'];
@@ -6,27 +8,69 @@ userNum = length(userData);
 
 plotNewGraph = cell(4,2);
 
-allDeviations = zeros(3,1);
-allCorrelations = zeros(3,1);
-
-%% Find Accuracy of Each User
+%% GET Z-SCORE
 
 for user=1:userNum
-    
-    % Record User Accuracy for Each of the 3 Shapes
-    totalAccuracy = zeros(3,1);
     
     % Convert eah Shape Structure into a 3x3 Array
     userName = userData(user).name;
     
     load(fullfile(filePath, userName, 'prevRelative.mat'));
     
-    order = load(fullfile(filePath, userName, 'order.mat'));
-    order = cell2mat(struct2cell(order));
+    shapeResponses = load(fullfile(filePath, userName, '/responses.mat'), 'responses');
+    shapeResponses = cell2mat(struct2cell(shapeResponses));
     
-    load(fullfile(filePath, userName, 'responses.mat'));
+        
+    trials = size(shapeResponses, 2);
+    binNum = ceil(trials/10);
     
-    trials = size(responses, 2);
+    shapeIndices = load(fullfile([filePath, userName, '/order.mat']), 'order');
+    shapeIndices = cell2mat(struct2cell(shapeIndices));
+    
+    userShapes = shapeResponses(1,:);
+    trueShapes = shapeResponses(2,:);
+    
+    % Find the indices of shapes 1-3
+    shapeDiffList = abs(shapeIndices(2:end) - shapeIndices(1:end-1));
+    %shapeDiffList = horzcat(0, shapeDiffList);
+    % Find where the user was correct or incorrect
+    correctList = userShapes == trueShapes;
+        
+    histList = vertcat(shapeDiffList, correctList);
+    histList = sortrows(histList', 1)';
+    bins = [1:binNum];
+    binLabels = zeros(binNum,2);
+    histVals = zeros(binNum, 1);
+    stdVals = zeros(binNum, 1);
+    for bin=1:binNum
+        histVals(bin) = mean(histList(2, 10*(bin-1)+1:10*bin));
+        stdVals(bin) = std(histList(2, 10*(bin-1)+1:10*bin));
+        
+        binLabels(bin, :) = [histList(1, 10*(bin-1)+1), histList(1, 10*bin)];
+    end
+        
+    hold on
+    
+    title('Difference in shape morphs, from one trial to the next, vs. user accuracy')
+	bar(bins, histVals);
+	xlabel('Differences between previous and current shape appearance, from least to greatest')
+	ylabel('Mean accuracy from 0 to 1')
+    
+    somenames = num2str(bins.*10);
+    xticks([1:1:binNum])
+    visualBins = cell(binNum,1);
+    for bin=1:binNum
+        visualBins{bin} = [num2str(binLabels(bin,1)), '-', num2str(binLabels(bin,2))];
+    end
+    xticklabels(visualBins);
+	%disp(histVals-stdVals);
+        
+	er = errorbar(bins,histVals,-stdVals,stdVals);    
+	er.Color = [0 0 0];                            
+	er.LineStyle = 'none';  
+    
+    %% GET Z-Scores
+    
     p0s = zeros(1,3); 
     phats = zeros(1,3);
     ns = zeros(1,3);
@@ -40,66 +84,4 @@ for user=1:userNum
     cdfs = normcdf(zs);
     Pvals = 1-cdfs;
     save(fullfile(filePath, userName, 'pvals.mat'), 'Pvals');
-    
-    %{
-    for shapeClass=1:3
-        
-        % Set up the two Metrics of Comparison
-        shapePrev = reshape(prevRelative(shapeClass,:,:), 3, 3);
-        
-        plotNewGraph{shapeClass,1} = figure;
-        bar3(shapePrev);
-        
-        allShapes = sum(reshape(shapePrev, 3^2, 1));
-        
-        ordIndices = find(responses(2,1:end-1) == shapeClass);
-        meanDeviation = mean(((3*order(ordIndices)/147) - responses(1, ordIndices+1)));
-        
-        if allShapes > 0
-            booldeanDiag = diag(repelem(1,3));
-
-            % Find where User Response deviates from set Shape Class
-            testXor = length(setxor(booldeanDiag, (shapePrev ./ shapePrev)));
-
-            % Record Accuracy as Percentage Right for each Shape
-            pctAccuracy = 1-(testXor/allShapes);
-            totalAccuracy(shapeClass) = pctAccuracy;
-
-            plotNewGraph{shapeClass} = figure;
-
-            lenRowOne = length(shapePrev(1,:)); 
-            lenRowTwo = length(shapePrev(2,:)); 
-            lenRowThree = length(shapePrev(3,:)); 
-
-            userX = zeros(1,1);
-            userY = zeros(1,1);
-
-            shapePrev = reshape(shapePrev, 3^2, 1);
-            trueIndices = find(shapePrev > 0);
-
-            userX(1:length(trueIndices)) = mod(trueIndices, 3)+1;
-            plotX = userX;
-            userY(1:length(trueIndices)) = 3-(floor(trueIndices/3)+1);
-            
-            [p,S] = polyfit(userX,userY,1); 
-            predictFit = polyval(p, userX);
-            
-            hold on
-            plot(plotX, predictFit);
-            scatter(userX, userY);
-            
-            r = corrcoef(userX, userY);
-            disp(['The correlation for Shape ' num2str(shapeClass) ...
-                ' is: ' num2str(r(1,2))]);
-            disp(['Mean Deviation is ' num2str(meanDeviation)]);
-            
-            allDeviations(shapeClass) = r(1,2);
-            allCorrelations(shapeClass) = meanDeviation;
-            
-            hold off
-        end
-    end
-    %}
 end
-
-        
