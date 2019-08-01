af4d2bb42ffacbd89b2e6171090db62a703129eb
UnsignedByte <edlam1889@gmail.com>
2019-08-01 14:34:14 -0700

    gw

--- tuuuuuuumor.m
+++ tuuuuuuumor.m
@@ -107,7 +107,8 @@ cd(init);
 save('prev.mat', 'prev'); %3x3x3 matrix
 save('prevRelative.mat', 'prevRelative');
 save('order.mat', 'order');
-
+save('corrchunks.mat', 'corrchunks');
+save('corrmags.mat', 'corrmags');
 save('responses.mat', 'responses');
 cd ../..;
 
